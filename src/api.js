82bc28b74734309e25bbc51f821b00ffe1f27263
Harsh kumar choudhary <harshkumarchoudhary144@gmail.com>
2023-05-28 11:51:00 +0530

    Added API Endpoint

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3005/api';
+const API_BASE_URL = 'https://management-api-anas.onrender.com/api';
 
 export const getInventory = async () => {
     try {
